acad5ff0c3440f461a02992ca3a43af7a0eddf76
Patrick Ullmann <codejunkie84@users.noreply.github.com>
2026-09-09 23:13:13 +0200

    ADD (remove/range): Unittests for edge cases

--- tests/command/remove/range_test.sh
+++ tests/command/remove/range_test.sh
@@ -27,3 +27,90 @@ function test_command_remove_range()
 
     assert_same "356" "$(pdftotext output.pdf - | tr -d '[:space:]')"
 }
+
+# /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function test_command_remove_range_page_first()
+{
+    # shellcheck disable=SC1091 # ROOT_DIR is provided by the bootstrap.
+    command_remove_range "${ROOT_DIR}/tests/_data/pages.pdf" 1 output.pdf
+
+    assert_same "output.pdf" "$(printf '%s\n' *)"
+
+    assert_same "2345678" "$(pdftotext output.pdf - | tr -d '[:space:]')"
+}
+
+# /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function test_command_remove_range_page_last()
+{
+    # shellcheck disable=SC1091 # ROOT_DIR is provided by the bootstrap.
+    command_remove_range "${ROOT_DIR}/tests/_data/pages.pdf" 8 output.pdf
+
+    assert_same "output.pdf" "$(printf '%s\n' *)"
+
+    assert_same "1234567" "$(pdftotext output.pdf - | tr -d '[:space:]')"
+}
+
+# /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function test_command_remove_range_page_0()
+{
+    # shellcheck disable=SC1091 # ROOT_DIR is provided by the bootstrap.
+    command_remove_range "${ROOT_DIR}/tests/_data/pages.pdf" 0 output.pdf
+
+    assert_not_same "output.pdf" "$(printf '%s\n' *)"
+}
+
+# /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function test_command_remove_range_page_negative()
+{
+    # shellcheck disable=SC1091 # ROOT_DIR is provided by the bootstrap.
+    command_remove_range "${ROOT_DIR}/tests/_data/pages.pdf" -1 output.pdf
+
+    assert_not_same "output.pdf" "$(printf '%s\n' *)"
+}
+
+# /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function test_command_remove_range_page_start_higher_than_end()
+{
+    # shellcheck disable=SC1091 # ROOT_DIR is provided by the bootstrap.
+    command_remove_range "${ROOT_DIR}/tests/_data/pages.pdf" 5-2 output.pdf
+
+    assert_not_same "output.pdf" "$(printf '%s\n' *)"
+}
+
+# /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function test_command_remove_range_all_pages()
+{
+    # shellcheck disable=SC1091 # ROOT_DIR is provided by the bootstrap.
+    command_remove_range "${ROOT_DIR}/tests/_data/pages.pdf" 1-8 output.pdf
+
+    assert_not_same "output.pdf" "$(printf '%s\n' *)"
+}
+
+# /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function test_command_remove_range_page_as_range()
+{
+    # shellcheck disable=SC1091 # ROOT_DIR is provided by the bootstrap.
+    command_remove_range "${ROOT_DIR}/tests/_data/pages.pdf" 5-5 output.pdf
+
+    assert_same "output.pdf" "$(printf '%s\n' *)"
+
+    assert_same "1234678" "$(pdftotext output.pdf - | tr -d '[:space:]')"
+}
+
+# /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function test_command_remove_range_not_existing_pages()
+{
+    # shellcheck disable=SC1091 # ROOT_DIR is provided by the bootstrap.
+    command_remove_range "${ROOT_DIR}/tests/_data/pages.pdf" 10-20 output.pdf
+
+    assert_not_same "output.pdf" "$(printf '%s\n' *)"
+}
+
+# /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function test_command_remove_range_partial_existing_pages()
+{
+    # shellcheck disable=SC1091 # ROOT_DIR is provided by the bootstrap.
+    command_remove_range "${ROOT_DIR}/tests/_data/pages.pdf" 5-20 output.pdf
+
+    assert_not_same "output.pdf" "$(printf '%s\n' *)"
+}
