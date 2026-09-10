5af16ece04e6f63cd3cb5774e8e641abeb34f33a
Patrick Ullmann <codejunkie84@users.noreply.github.com>
2026-09-10 22:10:17 +0200

    ADD (command/merge): test cases with not existing files

--- tests/command/merge/duplex_test.sh
+++ tests/command/merge/duplex_test.sh
@@ -30,3 +30,19 @@ function test_command_merge_duplex()
 
     assert_same "12345678" "$(pdftotext output.pdf - | tr -d '[:space:]')"
 }
+
+# /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function test_command_merge_duplex_with_not_existing_file()
+{
+    local exit_code=0
+
+    # shellcheck disable=SC1091     # ROOT_DIR is provided by the bootstrap.
+    command_merge_duplex \
+        "${ROOT_DIR}/tests/_data/duplex-frontside.pdf" \
+        "${ROOT_DIR}/tests/_data/unknown-file.pdf" \
+        output.pdf || exit_code=$?
+
+    assert_same "1" "${exit_code}"
+
+    assert_not_same "output.pdf" "$(printf '%s\n' *)"
+}
