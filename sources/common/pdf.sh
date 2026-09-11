e4b3b9d2dbe91308bd1aea04314f8f0a62f4ac91
Patrick Ullmann <codejunkie84@users.noreply.github.com>
2026-09-11 22:10:42 +0200

    ADD (common/pdf): unittests for common/pdf.sh (#9)

--- sources/common/pdf.sh
+++ sources/common/pdf.sh
@@ -16,6 +16,9 @@
 function _pdf_get_num_pages()
 {
     local -r input_file="${1}"
+
+    _assert_file "${input_file}" "Unable to determine the total number of pages" || return 1
+
     local -r num_pages=$(pdftk "${input_file}" dump_data | grep NumberOfPages | awk '{print $2}')
 
     _assert_not_empty "${num_pages}" "Unable to determine the total number of pages"
